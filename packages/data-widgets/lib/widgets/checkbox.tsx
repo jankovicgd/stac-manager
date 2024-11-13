cf190af462459ee24692e7dda6dc97dda50aae75
Daniel da Silva <danielfdsilva@gmail.com>
2024-11-13 17:55:22 +0000

    Fix radio and checkbox fields

--- packages/data-widgets/lib/widgets/checkbox.tsx
+++ packages/data-widgets/lib/widgets/checkbox.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
-import { Checkbox, Flex, FormControl, FormLabel } from '@chakra-ui/react';
-import { FastField, FastFieldProps } from 'formik';
+import {
+  Checkbox,
+  CheckboxGroup,
+  Flex,
+  FormControl,
+  FormLabel
+} from '@chakra-ui/react';
+import { useField } from 'formik';
 import {
   SchemaFieldArray,
   SchemaFieldString,
@@ -12,6 +18,8 @@ import { FieldLabel } from '../components/elements';
 export function WidgetCheckbox(props: WidgetProps) {
   const { pointer, isRequired } = props;
   const field = props.field as SchemaFieldArray<SchemaFieldString>;
+
+  const [{ value }, , { setValue, setTouched }] = useField(pointer);
 
   if (!field.items.enum?.length) {
     throw new Error('WidgetCheckbox: items.enum is required');
@@ -25,25 +33,19 @@ export function WidgetCheckbox(props: WidgetProps) {
         </FormLabel>
       )}
       <Flex gap={4}>
-        {field.items.enum.map(([label, checkboxVal]) => (
-          <FastField name={pointer} type='checkbox' key={label}>
-            {({
-              /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
-              field: { value: groupValues, checked, ...fieldProps }
-            }: FastFieldProps) => {
-              return (
-                <Checkbox
-                  {...fieldProps}
-                  size='sm'
-                  value={checkboxVal}
-                  isChecked={groupValues?.includes(checkboxVal)}
-                >
-                  {label}
-                </Checkbox>
-              );
-            }}
-          </FastField>
-        ))}
+        <CheckboxGroup
+          value={value}
+          onChange={(v) => {
+            setValue(v);
+            setTouched(true);
+          }}
+        >
+          {field.items.enum.map(([label, checkboxVal]) => (
+            <Checkbox key={label} size='sm' value={checkboxVal}>
+              {label}
+            </Checkbox>
+          ))}
+        </CheckboxGroup>
       </Flex>
     </FormControl>
   );
--- packages/data-widgets/lib/widgets/radio.tsx
+++ packages/data-widgets/lib/widgets/radio.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { FormControl, FormLabel, Radio, RadioGroup } from '@chakra-ui/react';
 import { SchemaFieldString, WidgetProps } from '@stac-manager/data-core';
-import { FastField } from 'formik';
+import { useField } from 'formik';
 
 import { FieldLabel } from '../components/elements';
 
 export function WidgetRadio(props: WidgetProps) {
   const { pointer, isRequired } = props;
   const field = props.field as SchemaFieldString;
+
+  const [{ value }, , { setValue, setTouched }] = useField<string>(pointer);
 
   if (!field.enum?.length) {
     throw new Error('WidgetRadio: enum is required');
@@ -20,18 +22,20 @@ export function WidgetRadio(props: WidgetProps) {
           <FieldLabel size='xs'>{field.label}</FieldLabel>
         </FormLabel>
       )}
-      <RadioGroup size='sm' gap={4} display='flex'>
-        {field.enum.map(([label, value]) => (
-          <FastField
-            key={label}
-            as={Radio}
-            name={pointer}
-            type='radio'
-            value={value}
-            size='sm'
-          >
+      <RadioGroup
+        size='sm'
+        gap={4}
+        display='flex'
+        value={value}
+        onChange={(v) => {
+          setValue(v);
+          setTouched(true);
+        }}
+      >
+        {field.enum.map(([label, radioValue]) => (
+          <Radio key={label} size='sm' value={radioValue}>
             {label}
-          </FastField>
+          </Radio>
         ))}
       </RadioGroup>
     </FormControl>
